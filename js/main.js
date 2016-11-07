2b671d766cd728e450386915e7a27fec2ad06b38
Petantse <pet.stoyanova@Petyas-MacBook-Pro.local>
2016-11-07 18:30:28 +0000

    changed structure

--- js/main.js
+++ js/main.js
@@ -1,1 +1,16 @@
-console.log('it works');
+$( document ).ready(function() {
+    // $('.skills').hide();
+    console.log( "it works" );
+
+    $('.blurred').mouseover(function(){
+      $('.blurred').addClass('blurred_hover').removeClass('animated');
+      $('.blurb-content').addClass('blurb-blurred');
+      $('.skills_hidden').addClass('skills_visible');
+    });
+    $('.blurred').mouseleave(function(){
+      $('.blurred').removeClass('blurred_hover').addClass('animated');
+      $('.blurb-content').removeClass('blurb-blurred');
+      $('.skills_hidden').removeClass('skills_visible');
+    });
+
+});
