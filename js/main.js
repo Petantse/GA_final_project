fc615d3dcae50cecb82d6f64ce4b20fec9e4fda9
Petantse <pet.stoyanova@Petyas-MacBook-Pro.local>
2016-11-07 18:58:59 +0000

    removed animation

--- js/main.js
+++ js/main.js
@@ -3,12 +3,12 @@ $( document ).ready(function() {
     console.log( "it works" );
 
     $('.blurred').mouseover(function(){
-      $('.blurred').addClass('blurred_hover').removeClass('animated');
+      $('.blurred').addClass('blurred_hover');
       $('.blurb-content').addClass('blurb-blurred');
       $('.skills_hidden').addClass('skills_visible');
     });
     $('.blurred').mouseleave(function(){
-      $('.blurred').removeClass('blurred_hover').addClass('animated');
+      $('.blurred').removeClass('blurred_hover');
       $('.blurb-content').removeClass('blurb-blurred');
       $('.skills_hidden').removeClass('skills_visible');
     });
